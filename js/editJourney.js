54e1207f373efbc2bb154d659b637078404583da
raeubert <simonroof@tutanota.com>
2021-11-16 19:01:38 +0100

    dataArray edited - still need to finish

--- js/editJourney.js
+++ js/editJourney.js
@@ -4,68 +4,65 @@ let dataArray = localStorage.getItem("trips")
 : [];
 const addButton = document.querySelector(".addTrip");
 let table = document.querySelector(".triptable");
-const btnWrapper = document.querySelector("#btnWrapper");
+//const btnWrapper = document.querySelector("#btnWrapper");
 const saveEditBtn = document.querySelector("#saveEdit");
 const cancelEditBtn = document.querySelector("#cancelEdit");
 
+//Reise-Dummys hinzufügen - init
+if(dataArray.length>=0){
+    var header = { 'trips': {'tripname': "Reisename", 'startDate': "Anfang", 'endDate': "Ende", 'country':"Reiseland"}};
+    var kubaTrip = { 'trips' : {'tripname': "Surfen & Entspannung", 'startDate': "07.09.2021", 'endDate': "14.09.2021", 'country': "Kuba"}};
+    var spainTrip = { 'trips' : {'tripname': "Spa-Woche", 'startDate': "02.10.2021", 'endDate': "08.10.2021", 'country': "Spanien"}};
+    var hungaryTrip = { 'trips' : {'tripname': "Erholung unter Palmen ", 'startDate': "05.01.2022", 'endDate': "12.01.2022", 'country': "Ungarn"}};
+    
+    dataArray.push(header);
+    dataArray.push(kubaTrip);
+    dataArray.push(spainTrip);
+    dataArray.push(hungaryTrip);
+    
+    for(var i=0; i< 1;i++){          //header
+        var row = table.insertRow(0);  
+        var headerCell1 = row.appendChild(document.createElement("TH"));
+        var headerCell2 = row.appendChild(document.createElement("TH"));
+        var headerCell3 = row.appendChild(document.createElement("TH"));
+        var headerCell4 = row.appendChild(document.createElement("TH"));
+        headerCell1.innerText = dataArray[0].trips.tripname;
+        headerCell2.innerText = dataArray[0].trips.startDate;
+        headerCell3.innerText = dataArray[0].trips.endDate;
+        headerCell4.innerText = dataArray[0].trips.country;
+    }
+
+    for(let i=1;i<4;i++){       //table data
+        var row = table.insertRow(i);
+        var cell1 = row.appendChild(document.createElement("td"));
+        var cell2 = row.appendChild(document.createElement("td"));
+        var cell3 = row.appendChild(document.createElement("td"));
+        var cell4 = row.appendChild(document.createElement("td"));
+        cell1.innerText = dataArray[i].trips.tripname;
+        cell2.innerText = dataArray[i].trips.startDate;
+        cell3.innerText = dataArray[i].trips.endDate;
+        cell4.innerText = dataArray[i].trips.country;
+    }
+}
+
 //falls bereits Daten in localstorage vorhanden - wichtig nach reload der Seite
-if(dataArray.length>0){
-    for(let i=0;i<dataArray.length;i++){
+if(dataArray.length > 4){
+    for(let i=0; i<(dataArray.length-4); i++){
+        console.log(4+i);
         var row = table.insertRow(4+i);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
     
-        cell1.appendChild(document.createTextNode(dataArray[i].trips.tripname));
-        cell2.appendChild(document.createTextNode(dataArray[i].trips.startDate));
-        cell3.appendChild(document.createTextNode(dataArray[i].trips.endDate));
-        cell4.appendChild(document.createTextNode(dataArray[i].trips.country));
+        cell1.appendChild(document.createTextNode(dataArray[4+i].trips.tripname));
+        cell2.appendChild(document.createTextNode(dataArray[4+i].trips.startDate));
+        cell3.appendChild(document.createTextNode(dataArray[4+i].trips.endDate));
+        cell4.appendChild(document.createTextNode(dataArray[4+i].trips.country));
     }
-}
+};
 
 /*
-//Reise-Dummys hinzufügen
-var genesisArray = [];
-
-var header = { 'trips': {0: "Reisename", 1: "Anfang", 2: "Ende",3:"Reiseland", 4:""}};
-var kubaTrip = { 'trips' : {'tripname': "Surfen & Entspannung", 'startDate': "07.09.2021", 'endDate': "14.09.2021", 'country': "Kuba"}};
-var spainTrip = { 'trips' : {'tripname': "Spa-Woche", 'startDate': "02.10.2021", 'endDate': "08.10.2021", 'country': "Spanien"}};
-var hungaryTrip = { 'trips' : {'tripname': "Erholung unter Palmen ", 'startDate': "05.01.2022", 'endDate': "12.01.2022", 'country': "Ungarn"}};
-
-genesisArray.push(header);
-genesisArray.push(kubaTrip);
-genesisArray.push(spainTrip);
-genesisArray.push(hungaryTrip);
-
-if(dataArray.length===0){
-    
-
-    // for(var i=0; i< 6;i++){          //funzt noch nicht ganz
-    // var row = table.insertRow(-1);//header
-    //     var headerCell = row.appendChild(document.createElement("TH"));
-    //     headerCell.innerText = genesisArray[0].trips[i];
-    // }
-
-    for(let i=0;i<5;i++){
-
-        window['editBtn'+i] = document.createElement("button");
-        window['delBtn'+i] = document.createElement("button");
-        var editT = document.createTextNode("Bearbeiten");
-        var delT = document.createTextNode("Löschen");
-
-        var cell1 = row.insertCell(0).appendChild(document.createTextNode(genesisArray[i].trips.tripname));
-        var cell2 = row.insertCell(1).appendChild(document.createTextNode(genesisArray[i].trips.startDate));
-        var cell3 = row.insertCell(2).appendChild(document.createTextNode(genesisArray[i].trips.endDate));
-        var cell4 = row.insertCell(3).appendChild(document.createTextNode(genesisArray[i].trips.country));
-        var cell5 = row.insertCell(4).appendChild(window['editBtn'+i]);
-        var cell6 = row.insertCell(5).appendChild(window['delBtn'+i]);
-        window['editBtn'+i].appendChild(editT);
-        window['delBtn'+i].appendChild(delT);
-    }
-}
-*/
-
 //Reisen bearbeiten     -> TODO: von localStorage aus zugreifen
 btnWrapper.addEventListener('click', (event) => {
     //checken ob Klick auf Button nicht auf div "btnWrapper"
@@ -73,6 +70,9 @@ btnWrapper.addEventListener('click', (event) => {
     if (!isButton) {
         return;
     }
+
+
+    /*  //eher unüblich für dynamische Variablen ->besser codepen/k-nut -> https://codepen.io/k-nut
     for(i=1; i<4; i++){
     if(event.target.id = window['editTrip-'+i]){   //dynamische Parameter für Trip2-3?? -> funktioniert nicht
         
@@ -124,6 +124,8 @@ btnWrapper.addEventListener('click', (event) => {
 }) 
 
 
+
+
 function openForm(){
     document.querySelector(".form-popup").style.display ="block";
     document.querySelector("body").style.background ="grey";
@@ -151,22 +153,23 @@ cancelEditBtn.addEventListener('click', (event) => {
     event.preventDefault();
     closeForm();
 })
+*/
 
 
 //um Reisen hinzuzufügen
 addButton.addEventListener('click', function(){
     
-    if(dataArray.length == 0){
+    if(dataArray.length === 4){
         var row = table.insertRow(4);
     }else{
-        var row = table.insertRow(4+dataArray.length);
+        var row = table.insertRow(dataArray.length);
     }
     
     let tripname = document.querySelector("#tripname").value;
     let startDate = document.querySelector("#startDate").value;
     let endDate = document.querySelector("#endDate").value;
     let country = document.querySelector("#country").value;
-    
+
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
