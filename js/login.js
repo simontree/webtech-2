16259329ea798689e8ca6bf266071e47f748278c
Dohruba <hendrichsricardo@gmail.com>
2021-12-19 22:56:03 +0100

    Login.js changes

--- js/login.js
+++ js/login.js
@@ -1,8 +1,23 @@
+//connect Frontend to Backend
+const BASE_URL = "http://localhost:5000";
+const loginButton = document.querySelector("#submit");
 
 function pwValidation(){
     var mail = document.querySelector('#email').value;
     var password = document.querySelector('#pw').value;
-s
+    var tableData = {
+        email: mail,
+        password: password,
+    }
+    const response = fetch(`${BASE_URL}/login`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tableData),
+      });
+      return response.status === 200;
     /*if (mail === "huehne@htw-berlin.de" && password === "hunter2"){
         window.location.replace("map.html"); 
     }else{
