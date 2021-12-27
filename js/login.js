a892aae8cd8dbc90b85860085c0f98733373a06d
simontree <simonroof@tutanota.com>
2021-12-27 17:55:55 +0100

    code rubbish deleted

--- js/login.js
+++ js/login.js
@@ -3,42 +3,34 @@
 const BASE_URL = "http://localhost:5000";
 
 const loginButton = document.querySelector(".loginBtn");
-//console.log(loginButton);
 
-loginButton.addEventListener("click", function(){
-    var mail = document.querySelector("#email").value;
-    var password = document.querySelector("#pw").value;
-    var tableData = {
-        email: mail,
-        password: password,
-    };
-    
-    const tryLogin = async () =>{
-        const response = await fetch(`${BASE_URL}/login`, {
-            method: "POST",
-            mode: "cors",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(tableData),
-        }).then((response) => response.json()).then((res) =>{
-            return res.status==200;
-        });
-        console.log(response);
-        if(response){
-            window.location.replace("map.html"); 
-        }else{
-            alert ("Falsche Login Daten");
-        }
-    }
-    tryLogin();
-    /*if (mail === "huehne@htw-berlin.de" && password === "hunter2"){
-            window.location.replace("map.html"); 
-        }else{
-            alert ("Falsche Login Daten");
-        }*/
+loginButton.addEventListener("click", function () {
+  var mail = document.querySelector("#email").value;
+  var password = document.querySelector("#pw").value;
+  var tableData = {
+    email: mail,
+    password: password,
+  };
+
+  const tryLogin = async () => {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tableData),
     })
-
-
-
-
+      .then((response) => response.json())
+      .then((res) => {
+        return res.status == 200;
+      });
+    console.log(response);
+    if (response) {
+      window.location.replace("map.html");
+    } else {
+      alert("Falsche Login Daten");
+    }
+  };
+  tryLogin();
+});
