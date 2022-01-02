b857fb14f936a21cab18208e8de8f961670cb073
Dohruba <hendrichsricardo@gmail.com>
2022-01-02 20:01:00 +0100

    Debugging

--- js/login.js
+++ js/login.js
@@ -30,7 +30,7 @@ loginButton.addEventListener("click", function () {
         let expires = "expires=" + d.toUTCString();
         let email = tableData.email;
         console.log(email);
-        document.cookie = "session=" + email + "==;" + expires + ";path=/";
+        document.cookie = "session=" + email + ";" + expires + ";path=/";
         window.location.replace("map.html");
     } else {
       alert("Falsche Login Daten");
